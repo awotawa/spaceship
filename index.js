1bdc4b482c4235e84f74efd9c6ea4df0959799a7
Alexandre Wotawa <awotawa@gmail.com>
2023-05-01 23:12:56 +0200

    feat: create GET for 1 astronaut

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ app.get("/astronauts", function (req, res) {
   res.status(200).json(astronauts);
   console.log(astronauts);
 });
+
+app.get("/astronauts/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  const astronaut = astronauts.find((astronaut) => astronaut.id === id);
+  console.log(astronaut);
+  if (astronaut === undefined) {
+    res.status(404).json("The requested astronaut was not found.");
+  } else {
+    res.status(200).json(astronaut);
+  }
+});
